cfc82166e1cd3d9784c0b5319d721698e991d9d5
Philipp <philipp.lackner@yahoo.de>
2020-04-30 18:41:25 +0200

    Realtime Updates

--- app/src/main/java/com/androiddevs/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/androiddevs/firebasefirestore/MainActivity.kt
@@ -29,8 +29,27 @@ class MainActivity : AppCompatActivity() {
             savePerson(person)
         }
 
-        btnRetrieveData.setOnClickListener {
+        subscribeToRealtimeUpdates()
+
+        /*btnRetrieveData.setOnClickListener {
             retrievePersons()
+        }*/
+    }
+
+    private fun subscribeToRealtimeUpdates() {
+        personCollectionRef.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            firebaseFirestoreException?.let {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            querySnapshot?.let {
+                val sb = StringBuilder()
+                for(document in it) {
+                    val person = document.toObject<Person>()
+                    sb.append("$person\n")
+                }
+                tvPersons.text = sb.toString()
+            }
         }
     }
 
